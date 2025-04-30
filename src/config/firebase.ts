c8caa1e9a33be929c10aa27c5b443553df06db51
phorsphaktra <phorsphaktra99@gmail.com>
2025-04-30 16:04:49 +0700

    deploy for prod

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,27 +4,27 @@ import { getAuth } from 'firebase/auth';
 
 
 //for UAT
-const firebaseConfig = {
-  apiKey: "AIzaSyAwjjKYJ38mKpJz1AcsouYgCWer3Jc9lQY",
-  authDomain: "money-tracker-e9a76.firebaseapp.com",
-  projectId: "money-tracker-e9a76",
-  storageBucket: "money-tracker-e9a76.firebasestorage.app",
-  messagingSenderId: "629369864190",
-  appId: "1:629369864190:web:c1f88983919ec1bad40aaf",
-  measurementId: "G-HJTP1C5FPE"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAwjjKYJ38mKpJz1AcsouYgCWer3Jc9lQY",
+//   authDomain: "money-tracker-e9a76.firebaseapp.com",
+//   projectId: "money-tracker-e9a76",
+//   storageBucket: "money-tracker-e9a76.firebasestorage.app",
+//   messagingSenderId: "629369864190",
+//   appId: "1:629369864190:web:c1f88983919ec1bad40aaf",
+//   measurementId: "G-HJTP1C5FPE"
+// };
 
 
 //for PROD
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCZyZepVAQR6ABw1k_EfyucN6JcjWPjyoU",
-//   authDomain: "money-tracker-1256.firebaseapp.com",
-//   databaseURL: "https://money-tracker-1256-default-rtdb.firebaseio.com",
-//   projectId: "money-tracker-1256",
-//   storageBucket: "money-tracker-1256.firebasestorage.app",
-//   messagingSenderId: "599127425663",
-//   appId: "1:599127425663:web:517e77baf6dcba7265754a"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCZyZepVAQR6ABw1k_EfyucN6JcjWPjyoU",
+  authDomain: "money-tracker-1256.firebaseapp.com",
+  databaseURL: "https://money-tracker-1256-default-rtdb.firebaseio.com",
+  projectId: "money-tracker-1256",
+  storageBucket: "money-tracker-1256.firebasestorage.app",
+  messagingSenderId: "599127425663",
+  appId: "1:599127425663:web:517e77baf6dcba7265754a"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
