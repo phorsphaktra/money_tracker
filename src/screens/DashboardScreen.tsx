6b006bac880afbb75231d3b2c1b088fedf0fd890
phorsphaktra <phorsphaktra99@gmail.com>
2025-05-17 09:59:39 +0700

    improve tranaction

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -275,7 +275,7 @@ export const DashboardScreen = () => {
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 space-y-8">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 space-y-6">
       <header>
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-3xl font-bold text-gray-900 dark:text-white tracking-tight">
